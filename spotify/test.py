b4819f6cce450bd93fa1ce9b58a224ef945104f3
githubuser201 <tommy.heg@gmail.com>
2020-04-28 16:17:44 -0400

    added comments in test file, problem with login

--- spotify/test.py
+++ spotify/test.py
@@ -5,33 +5,70 @@ import json
 import os
 import webbrowser
 
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-# artist= 'Moses Sumney'
-# track= 'Lonely World'
-# track_id = spotify.search(q='artist:' + artist + ' track:' + track, type='track')
-# print(track_id['tracks']['items'][0]['id'])
 
-# track_uri='spotify:track:5CQ30WqJwcep0pYcV4AMNc'
+def get_credentials():
+    # need client id and client secret for this
+    # set environment variables or pass them in
+    # environment variables: SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET
+    # these are gotten from the 'my dashboard' section on spotify for developers
+    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+    return spotify
 
-# token='BQA-__GPI7T1TQ6i3ACTfaTf4gbouX6Bt1LBJJkQgxkX2kU8EVlmvkBF5OGZ_B8gzd0EO8qhpqODvtIE3xMnpJU4MoktrPJ8X19F4wFqoWFjZk3hTpE7QqT2ESwE4mSQqjBqULhQa8xTstnYbOXyiB81WdklLrWz-_8JU3nd4X0nIzOaZTDLiUJfB6IuWLtmgxRLqXIOx3r1hd5S9knOHAkg5MuSzvo0mcQ9GFa2NEKJB0qMIX5r8ve19S-6RDyHSUYduoaX70oDl0l9jA'
-# token='BQDeStN0a-dYiVCLH87Lfhqte0ywSm8ATWc4cvKIg3ZieXCGknIsdkxUmYuqmyhmNzJL8MA-PUuZTOnwkJXpUm8ONZqtiGjMM7gC2JEGyeWxoPSedEixYro9exu0bmh-TGh8H_ehZKrhgM78n5cAx-8e6PsXW5d3StgSBDaATyRlvp1IG43UdxnCovEs4KSkoW67LtahhYQq-_WsbBnRRKfh7eMqio5GfQ_UcVw6tg7DQKcPducip84NeMzqKFldfFKASJG4_yPOZ2JKlQ' \
 
-# header={"Accept": "application/json", "Content-Type": "application/json", "Authorization": "Bearer "+token}
-# url='https://api.spotify.com/v1/me/player/devices'
-# response = r.request("GET", url, headers=header)
-# json_data =json.loads(response.text)
-# print(json_data['devices'][0]['id'])
-# device_id='13f7a36f018c035b9d6dd7de8469d71e62610e69'
-# url='https://api.spotify.com/v1/me/player/play'+'?device_id='+device_id
-# data='{\"uris\": [\"spotify:track:5CQ30WqJwcep0pYcV4AMNc\"] }'
-# response=r.request("PUT", url, data=data, headers=header)
-# print(response)
+def get_device_id(header):
+    # this gets the user devices- need to get device id for play
+    url = 'https://api.spotify.com/v1/me/player/devices'
+    response = r.request("GET", url, headers=header)
+    json_data = json.loads(response.text)
+    device_id = json_data['devices'][0]['id']
+    return device_id
 
-# need to add environment variable for this to work. forget what it was tho
-url = 'https://accounts.spotify.com/authorize?client_id=' + os.environ['SPOTIPY_CLIENT_ID'] + 'response_type=code' + \
-      'redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&scope=user-read-private%20user-read-email&state=34fFs29kd09'
-response = r.request("GET", url)
-print(response.text)
-file = open('login.html', 'w')
-file.write(response.text)
-webbrowser.open('login.html')
+
+def play_song(header, track_uri, device_id):
+    # this plays a song on that device
+    # get track uri from spotify song link
+    url = 'https://api.spotify.com/v1/me/player/play' + '?device_id=' + device_id
+    data = '{\"uris\": [\"' + track_uri + '\"] }'
+    response = r.request("PUT", url, data=data, headers=header)
+    return response
+
+
+def login():
+    # this is a login page that pops up
+
+    # keep getting this error tho---
+    # ERROR for site owner:
+    # Invalid domain for site key
+    # it also has a recaptcha for some reason? look into this
+    # https://formidableforms.com/knowledgebase/recaptcha/
+
+    # this link is what we want to do (i think)
+    # https://medium.com/@sedwardscode/creating-a-spotify-app-on-the-spotify-developer-page-16907b5872e8
+
+    url = 'https://accounts.spotify.com/authorize?client_id=' + os.environ['SPOTIPY_CLIENT_ID'] + 'response_type=code' + \
+          'redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&scope=user-read-private%20user-read-email&state' \
+          '=34fFs29kd09 '
+    response = r.request("GET", url)
+    print(response.text)
+    file = open('login.html', 'w')
+    file.write(response.text)
+    webbrowser.open('login.html')
+
+
+# get credentials first
+spotipy = get_credentials()
+
+# this is the token for getting user's devices
+# https://developer.spotify.com/console/get-users-available-devices/
+device_token = 'BQALeWT04abQ3XSfUFZ3v8NyLiF8vTJSCwHGm1iw-xGsik158JqaiLHsVIV78hIzoSU1HnpXoyNJ9ve-' \
+               'tt8YMzltOWgaDRA60S0y1CRELXD5cTXVGMqHmxRTELCS1T8u0oSs0bNwjL0oQoICU8KWJfxcM-Z8-ADz5L' \
+               'AQNcRqClYN5wOTKXdP3r-YQbvr7-XLzoUgZTs5LZ5mROWu1rK3tCFC_EuHC0mma46V1S_DRfqB31v56zZ4UMps' \
+               '01HGq7cBa0GCvXrKBmUQ2DPJwA'
+header = {"Accept": "application/json", "Content-Type": "application/json",
+          "Authorization": "Bearer " + device_token}
+device_id = get_device_id(header)
+track_uri = 'spotify:track:68KdZxeBL5tGtpVPgVjgzU'
+response = play_song(header, track_uri, device_id)
+print(response)
+
+login()
